3e1569717f4d1120a0662a4b377c25c3f5dca506
zeamanuel fetene <zeamanuel1994@gmail.com>
2025-07-13 12:07:20 +0300

    refactor: optimize Hero component structure and improve readability

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 const images = [
   "./images/Hero-img1.png",
   "./images/Hero-img2.png",
-  "./images/Hero-img3.png", 
+  "./images/Hero-img3.png",
+  "./images/masiko.png",
 ];
 
 export default function Hero({ onReservationClick }) {
