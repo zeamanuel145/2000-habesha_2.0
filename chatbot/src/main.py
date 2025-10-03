5eee62561ed74871d4f84b6cadae82ba6a950151
Eric Theuri <tericmail001@gmail.com>
2025-10-03 14:56:19 +0300

    Modified port

--- chatbot/src/main.py
+++ chatbot/src/main.py
@@ -122,5 +122,5 @@ async def chatbot(request: ChatRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8003))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
