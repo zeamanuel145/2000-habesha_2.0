a7dc240a961bf787f0c26b3c4c67f3adc7122a6c
zeamanuel fetene <zeamanuelfetene1994@gmail.com>
2025-12-31 04:20:01 -0800

    Updated Map component to change border style from 0 to 2 for improved visual appearance.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ export default function Map() {
         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d8680.49562191721!2d38.780235794644156!3d9.006333055163887!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2set!4v1750943454124!5m2!1sen!2set"
         width="100%"
         height="450"
-        style={{ border: 0 }}
+        style={{ border: 2 }}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
         className="rounded-lg shadow-lg w-full"
